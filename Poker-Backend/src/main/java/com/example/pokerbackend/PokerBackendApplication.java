a62c1995f68ba915e800e04eca18027314eec200
Simon Schmadlbauer <sschmadl@htl-steyr.ac.at>
2025-02-28 17:59:58 +0100

    Poker Backend project setup

---
+++ Poker-Backend/src/main/java/com/example/pokerbackend/PokerBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.pokerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PokerBackendApplication.class, args);
+	}
+}
---
+++ Poker-Backend/src/test/java/com/example/pokerbackend/PokerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

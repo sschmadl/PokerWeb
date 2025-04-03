58990259f51d14285a0ef4d7a09908ecf171a825
Simon Schmadlbauer <sschmadl@htl-steyr.ac.at>
2025-04-03 09:19:27 +0200

    renamed file

--- Poker-Backend/src/main/java/com/example/pokerbackend/handler/GlobalChatWebsocketHandler.java
+++ Poker-Backend/src/main/java/com/example/pokerbackend/handler/GlobalChatWebsocketHandler.java
@@ -2,6 +2,6 @@ package com.example.pokerbackend.handler;
 
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-public class MyWebSocketHandler extends TextWebSocketHandler {
+public class GlobalChatWebsocketHandler extends TextWebSocketHandler {
 
 }

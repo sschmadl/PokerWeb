38e9897f36be0964c552337eb29e1eba0144b3b4
Leon Arbeithuber <larbeith@htl-steyr.ac.at>
2025-04-07 09:32:41 +0200

    Basic Poker Table

--- Poker-frontend/pages/poker-game.vue
+++ Poker-frontend/pages/poker-game.vue
@@ -3,10 +3,19 @@
 const tableWidth = ref(window.innerWidth / 2.5);
 const tableHeight = ref(window.innerHeight / 2);
 
+const cards = ref([
+  { frontImage: '/cards_default/AS.svg', faceDown: false, highlighted: false },
+  { frontImage: '/cards_default/2S.svg', faceDown: false, highlighted: false },
+  { frontImage: '/cards_default/3S.svg', faceDown: false, highlighted: false },
+  { frontImage: '/cards_default/4S.svg', faceDown: false, highlighted: false },
+  { frontImage: '/cards_default/5S.svg', faceDown: false, highlighted: false },
+]);
+const cardHeight = ref(tableHeight.value / 3);
 
 function updateSizes() {
   tableWidth.value = window.innerWidth / 2.5;
   tableHeight.value = window.innerHeight / 2;
+  cardHeight.value = tableHeight.value / 3;
 }
 
 onMounted(() => {
@@ -16,6 +25,12 @@ onMounted(() => {
 onBeforeUnmount(() => {
   window.removeEventListener('resize', updateSizes);
 });
+
+function flipCards() {
+  cards.value.forEach((card) => {
+    card.faceDown = !card.faceDown;
+  })
+}
 
 </script>
 
@@ -30,12 +45,26 @@ onBeforeUnmount(() => {
       <div class="middle-part poker-table" :style="{
         width: tableWidth + 'px',
         height: tableHeight + 'px',
-      }"></div>
+      }">
+      </div>
       <div class="right-edge rounded-full poker-table" :style="{
         width: tableHeight + 'px',
         height: tableHeight + 'px',
         marginLeft: tableWidth + 'px',
       }"></div>
+      <div class="community-cards">
+        <Card
+            v-for="(card, index) in cards"
+            :key="index"
+            :face-down="card.faceDown"
+            :front-image="card.frontImage"
+            :height="cardHeight"
+            :highlighted="card.highlighted"
+        />
+      </div>
+      <UButton @click="flipCards" :style="{
+        zIndex: 15,
+      }">Flip</UButton>
     </div>
   </div>
 </template>
@@ -59,12 +88,21 @@ onBeforeUnmount(() => {
 .poker-table {
   background: #35654D;
   position: absolute;
-  z-index: 1;
 }
 
 .left-edge,
 .right-edge {
   border-radius: 50%;
+  z-index: -1;
+}
+
+.community-cards {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  user-select: none;
+  pointer-events: none;
 }
 
 </style>
